892d75c1356e7271cc04c386f5aa72545cb22992
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2020-11-17 14:37:00 +0100

    Update main.js

--- editable-article-watch/js/main.js
+++ editable-article-watch/js/main.js
@@ -2,12 +2,32 @@
 let article = document.querySelector('.content');
 
 
-// observe article for changes
+// ------------------------------------------------------------
+
+// First possibility -> keyup event listener on the article
+article.addEventListener("keyup", handleKeyup)
+
+function handleKeyup(e) {
+  console.log("----------------------------------------");
+  console.log("This is the key event:");
+  console.log(e);
+  console.log("This is the new caret position");
+  console.log(window.getSelection());
+}
+
+// ------------------------------------------------------------
+
+// Second possibility -> observe article for changes
 let articleObserver = new MutationObserver(handleChange);
 let options = {childList:true, attributes:true, subtree:true, characterData:true};
-
 articleObserver.observe(article, options);
 
 function handleChange(changes) {
+  console.log("----------------------------------------");
+  console.log("This has changed just now:");
   console.log(changes);
+  console.log("This is the new caret position");
+  console.log(window.getSelection());
 }
+
+
